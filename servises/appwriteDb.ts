08946f25ed7bae795779fc6fd869ee4498ffaece
natalia646 <nataxash46@gmail.com>
2026-07-08 16:50:13 +0300

    feat: implement search count update and trending movies fetching functionality

--- servises/appwriteDb.ts
+++ servises/appwriteDb.ts
@@ -1,3 +1,60 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATEBASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+const PROJECT_ID = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!;
+
+export const client = new Client()
+  .setProject(PROJECT_ID)
+  .setEndpoint("https://fra.cloud.appwrite.io/v1");
+
+const database = new Databases(client);
+
 export const updateSearchCount = async (query: string, movie: Movie) => {
-    
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        },
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: query,
+        count: 1,
+        movie_id: movie.id,
+        title: movie.title,
+        posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error("Error updating search count:", error);
+    throw error;
+  }
+};
+
+export const getTrendingMovies = async (): Promise<
+  TrendingMovie[] | undefined
+> => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+    return undefined;
+  }
 };
