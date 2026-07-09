c30d4bc78ff5893cca2f4774a66bb98cbaf9e68f
natalia646 <nataxash46@gmail.com>
2026-07-09 13:00:35 +0300

    fix: add padding to PageLayout component for improved layout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -4,7 +4,7 @@ import { View, Image } from "react-native";
 
 export const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <View className="flex-1 bg-primary">
+    <View className="flex-1 bg-primary px-1.5">
       <Image source={images.bg} className="w-full absolute z-0" />
       {children}
     </View>
